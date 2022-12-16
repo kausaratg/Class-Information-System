115bf8e35fc6065d149e78a537ca53cc22bf34a6
kassy <kausaratg@gmail.com>
2022-12-17 00:32:07 +0100

    Added group A, B, D timetable

--- timetable/urls.py
+++ timetable/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from timetable.views import timetable, timetable_c
+from timetable.views import timetable, timetable_c, timetable_a, timetable_d, timetable_b
 
 urlpatterns = [
     path("timetable", timetable, name="timetable"),
+    path("timetable_a", timetable_a, name="timetable_a"),
+    path("timetable_d", timetable_d, name="timetable_d"),
     path("timetable_c", timetable_c, name="timetable_c"),
+    path("timetable_b", timetable_b, name="timetable_b"),
 ]
--- timetable/views.py
+++ timetable/views.py
@@ -17,7 +17,7 @@ def timetable_c(request):
     textob.setFont("Helvetica", 14)
 
     lines = [
-        "Computer Science Group c Timetable ",
+        "Computer Science Group C Timetable ",
         " ",
         "Monday",
         "8am-8:50am MATH-D007/D104",
@@ -53,3 +53,142 @@ def timetable_c(request):
     c.save()
     buf.seek(0)
     return FileResponse(buf, as_attachment=True, filename="Group C timetable.pdf")
+
+
+def timetable_d(request):
+    buf = io.BytesIO()
+    c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+    textob = c.beginText()
+    textob.setTextOrigin(inch, inch)
+    textob.setFont("Helvetica", 14)
+
+    lines = [
+        "Computer Science Group D Timetable ",
+        " ",
+        "Monday",
+        "8:00am-9:00am Phy101 SCLT",
+        "10:00am-11:00am GEDS 105",
+        "11:00am-12:00pm CHEM 101(WRA/SCLT)",
+        "2:00pm-4:00pm STAT 101 A008/A007",
+        "4:00pm-6:00pm GEDS 101",
+        "  ",
+        "Tuesday",
+        "9:00am-10:00am GEDS 101(Amphitheatre)",
+        "10:00am-11:00am CHEM 101 (WRA/SCLT)",
+        "11:00am-1:00pm GEDS 131",
+        "2:00pm-3:00pm Phy 101 (SCLT)",
+        " ",
+        "Wednesday",
+        "9:00am-11:00am COSC 111 G1(E102)",
+        "11:00am-1:00pm COSC 111 G2(R1/8)",
+        "2:00pm-3:00pm  COSC 111 G2(R1/8)",
+        " ",
+        "Thursday",
+        "7:00am-9:00am COSC 107 (NH S4)",
+        "9:00am-11:00am GEDS 107",
+        "12:00am-1:00pm Chem Practical (D004)",
+        " ",
+        "Friday",
+        "8:00am-9:00am COSC 111 G1 (E102)",
+    ]
+    for line in lines:
+        textob.textLine(line)
+    c.drawText(textob)
+    c.showPage()
+    c.save()
+    buf.seek(0)
+    return FileResponse(buf, as_attachment=True, filename="Group D timetable.pdf")
+
+
+def timetable_a(request):
+    buf = io.BytesIO()
+    c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+    textob = c.beginText()
+    textob.setTextOrigin(inch, inch)
+    textob.setFont("Helvetica", 13)
+
+    lines = [
+        "Computer Science Group A Timetable ",
+        " ",
+        "Monday",
+        "7:00am-9:00am STAT 101 SCLT",
+        "10:00am-11:00am GEDS 105",
+        "11:00am-12:00pm PHY101 B107",
+        "2:00pm-4:00pm CHEM 101 B007",
+        "4:00pm-6:00pm GEDS 101",
+        "  ",
+        "Tuesday",
+        "9:00am-10:00am Chapel Seminar(Amphitheatre)",
+        "10:00am-11:00am CHEM 101 (A008)",
+        "11:00am-1:00pm GEDS 131",
+        "4:00pm-6:00pm MATH 101 B006",
+        " ",
+        "Wednesday",
+        "7:00am-9:00am COSC 107 NH Studio 4",
+        "11:00am-12:00pm MATH 101 B106",
+        "12:00pm-1:00pm  CHEM 101 PRACTICAL D004",
+        "3:00pm-4:00pm COSC 111(GRP 2) NH ROOM2",
+        " ",
+        "Thursday",
+        "9:00am-11:00am GEDS 107",
+        "11:00am-12:00pm COSC 111 GR1/NELE 202 ",
+        "2:00pm-4:00pm COSC 111 GR2 BUCODEL LAB 5"
+        " ",
+        "Friday",
+        "8:00am-10:00am PHY 101 SCLT",
+        "12:00pm-1:00pm COSC 111 GR1 F202",  
+    ]
+    for line in lines:
+        textob.textLine(line)
+    c.drawText(textob)
+    c.showPage()
+    c.save()
+    buf.seek(0)
+    return FileResponse(buf, as_attachment=True, filename="Group A timetable.pdf")
+
+def timetable_b(request):
+    buf = io.BytesIO()
+    c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+    textob = c.beginText()
+    textob.setTextOrigin(inch, inch)
+    textob.setFont("Helvetica", 13)
+
+    lines = [
+        "Computer Science Group B Timetable ",
+        " ",
+        "Monday",
+        "7:00am-9:00am CHEM 107 B007",
+        "10:00am-11:00am GEDS 105",
+        "11:00am-1:00pm MATH 101 D104",
+        "2:00pm-4:00pm PHY 101 F105",
+        "4:00pm-6:00pm GEDS 101",
+        "  ",
+        "Tuesday",
+        "9:00am-10:00am Chapel Seminar(Amphitheatre)",
+        "11:00am-12:00pm GEDS 131",
+        "12:00pm-1:00pm COSC111 GROUP E2O2",
+        "2:oopm-3:00pm MATH 101 B007",
+        "3:00pm-5:00pm STAT 101 BUCODEL",
+        " ",
+        "Wednesday",
+        "2:00pm-3:00pm PHY 101 B007",
+        "3:00pm-4:00pm COSC 111 GROUP 2 PRACT NEW HORIZON(ROOM 1)",
+        "4:00pm-6:00pm COSC 107 STUDIO 4",
+        " ",
+        "Thursday",
+        "8:00am-9:00am CHEM 101 B006",
+        "9:00am-11:00am GEDS 107 ",
+        "11:00am-12:00pm CHEM 101 PRACTICAL BOO7",
+        "2:00pm-3:00pm COSC 111 GROUP 1 E202",
+        "2:00PM-4:00PM COSC 111 GROUP 2 A112",
+        " ",
+        "Friday",
+        "9:00am-10:00am COSC 111 GROUP 1 PRACTICAL", 
+    ]
+    for line in lines:
+        textob.textLine(line)
+    c.drawText(textob)
+    c.showPage()
+    c.save()
+    buf.seek(0)
+    return FileResponse(buf, as_attachment=True, filename="Group B timetable.pdf")
